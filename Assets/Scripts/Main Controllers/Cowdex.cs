76c650d1d7fffc93cd5bf14a0cd408001ee05b2a
Andrea Buzzi <aby.development@outlook.it>
2023-07-08 21:00:07 +0200

    Monosingleton + indexed Cow

--- Assets/Scripts/Main Controllers/Cowdex.cs
+++ Assets/Scripts/Main Controllers/Cowdex.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cowdex : MonoBehaviour
+public class Cowdex : MonoSingleton<Cowdex>
 {
     // Start is called before the first frame update
     void Start()
