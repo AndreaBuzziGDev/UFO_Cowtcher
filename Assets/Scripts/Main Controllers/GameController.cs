edc034f541dc45147582008d15ffd015fc208f12
Andrea Buzzi <aby.development@outlook.it>
2023-07-10 01:18:27 +0200

    Worked on several sub-features, handled game states and a few minor things

--- Assets/Scripts/Main Controllers/GameController.cs
+++ Assets/Scripts/Main Controllers/GameController.cs
@@ -2,8 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class GameController : MonoSingleton<GameController>
 {
-    //EMPTY CLASS
-    
+    //ENUMS
+    public enum EGameState
+    {
+        Start,
+        Playing,
+        Paused
+    }
+
+    //DATA
+    private EGameState state = 0;
+
+    public bool IsPaused { get { return this.state == EGameState.Paused; } }
+
+
+    //METHODS
+    //...
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SHOULD HELP DRIVING THE OTHER CONTROLLERS THROUGH THEIR INITIALIZATION SEQUENTIALLY.
+
+    }
+
+
+
+    //FUNCTIONALITIES
+    //...
+    public void SetState(EGameState targetState)
+    {
+        state = targetState;
+        switch (state)
+        {
+            case EGameState.Start:
+                //RESERVED FOR INITIALIZATION
+
+                //TODO: TRANSITION TO STATE "PLAYING" AT THE END OF START STATE
+                break;
+
+            case EGameState.Playing:
+                UnpauseGame();
+
+
+                break;
+
+            case EGameState.Paused:
+
+                PauseGame();
+                break;
+
+        }
+    }
+
+
+    //PAUSING
+    public static void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    public static void UnpauseGame()
+    {
+        Time.timeScale = 1;
+    }
+
 }
---
+++ Assets/Scripts/Main Controllers/UIController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    //NB: THERE MIGHT BE AN "EXPLOSION" OF UI COMPONENTS BASED ON NECESSITY.
+    //THIS CLASS IS MEANT TO HANDLE BASIC UI. IF NECESSARY, SWITCH TO SOMETHING THAT ACTS LIKE A "TOP-LEVEL" MANAGER
+    //WHILE SOME OTHER SUB-COMPONENTS HANDLE THE DETAILS OF THE CODE.
+
+    //DATA
+    //TODO: MIGHT BE SIMPLIFIED BY USING DEDICATED CLASSES
+    [SerializeField] private List<GameObject> AllMenuPanels;
+
+    [SerializeField] private GameObject PauseGamePanel;
+    [SerializeField] private GameObject CowdexPanel;
+
+
+    //METHODS
+
+    //FUNCTIONALITIES
+
+
+    //GUI PANELS MANAGEMENT
+
+    ///HIDE ALL
+    public void HideAllMenuPanels()
+    {
+        foreach (GameObject go in AllMenuPanels)
+        {
+            if (go != null) go.SetActive(false);
+        }
+    }
+
+    ///INDIVIDUALS
+    //PAUSE
+    public void ShowPause() => PauseGamePanel.SetActive(true);
+    public void Hidepause() => PauseGamePanel.SetActive(false);
+
+    //COWDEX
+    public void ShowCowdex() => CowdexPanel.SetActive(true);
+    public void HideCowdex() => CowdexPanel.SetActive(false);
+
+
+
+}
