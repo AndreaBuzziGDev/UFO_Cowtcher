b46ee146cf7da42fb90f7c80fe88f298175ee1c2
Andrea Buzzi <aby.development@outlook.it>
2023-07-11 22:39:39 +0200

    Awake code implementation

--- Assets/Scripts/Cow.cs
+++ Assets/Scripts/Cow.cs
@@ -19,12 +19,8 @@ public class Cow : MonoBehaviour
     private State currentState = State.Calm;
     public State CurrentState { get { return currentState; } }
 
-    private ScriptableCow cowTemplate;
+    [SerializeField] private ScriptableCow cowTemplate;
     private Hideout targetHideout;
-
-    ///TIMERS
-    //TODO: IMPLEMENT
-
 
 
 
@@ -34,13 +30,23 @@ public class Cow : MonoBehaviour
     private ScriptableCow.Rarity rarity;
 
 
-
     /// SIMPLE DATA
+    private string cowName;
+    public string CowName { get { return cowName; } }
+
     private int fuelRecoveryAmount;
     public int FuelRecoveryAmount { get { return fuelRecoveryAmount; } }
 
     private float AlertRadius;
-    private float Speed;
+    private float SpeedCalm;
+    private float SpeedAlert;
+    private float Score;
+
+    ///TIMERS
+    private float TimerAlertToCalm;
+    private float TimerAlertToPanic;
+    private float TimerCalmMovement;
+    private float TimerCalmStill;
 
 
 
@@ -49,8 +55,8 @@ public class Cow : MonoBehaviour
     private List<SpawnPoint.Type> AllowedSpawnPointTypes = new();
 
 
-    private AbstractStatusAlteration statusAlteration;
-    public AbstractStatusAlteration Alteration { get { return statusAlteration; } }
+    private AbstractAlteration.EBuffType alteration;
+    public AbstractAlteration.EBuffType Alteration { get { return alteration; } }
 
 
     //TODO: EVALUATE FURTHER DIVERSIFICATION OF MOVEMENT PATTERNS
@@ -58,12 +64,44 @@ public class Cow : MonoBehaviour
     private AbstractMovementPattern movPatternAlert;
 
 
+    //TECHNICAL DATA FOR OTHER PURPOSES
+    private SpriteRenderer sr;
+
+
+
+
 
     //METHODS
     //...
     private void Awake()
     {
+        /// SIMPLE DATA
         //TODO: CLONE DATA FROM SCRIPTABLE COW
+        this.UID = cowTemplate.UID;
+        this.rarity = cowTemplate.rarity;
+        this.cowName = cowTemplate.Name;
+        this.fuelRecoveryAmount = cowTemplate.FuelRecoveryAmount;
+        this.AlertRadius = cowTemplate.AlertRadius;
+        this.SpeedCalm = cowTemplate.SpeedCalm;
+        this.SpeedAlert = cowTemplate.SpeedAlert;
+        this.Score = cowTemplate.Score;
+        this.TimerAlertToCalm = cowTemplate.TimerAlertToCalm;
+        this.TimerAlertToPanic = cowTemplate.TimerAlertToPanic;
+        this.TimerCalmMovement = cowTemplate.TimerCalmMovement;
+        this.TimerCalmStill = cowTemplate.TimerCalmStill;
+
+        /// COMPLEX DATA
+        this.FavouriteHideoutTypes = cowTemplate.FavouriteHideoutTypes;
+        this.AllowedSpawnPointTypes = cowTemplate.AllowedSpawnPointTypes;
+        this.alteration = cowTemplate.Alteration;
+        this.movPatternCalm = cowTemplate.movPatternCalm;
+        this.movPatternAlert = cowTemplate.movPatternAlert;
+        
+
+        //OTHER TECHNICAL AWAKE SETUP
+        sr = this.gameObject.GetComponent<SpriteRenderer>();
+        sr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        sr.receiveShadows = true;
 
     }
 
