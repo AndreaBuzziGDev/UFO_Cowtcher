edb3eb80751303a90fe7b94425aade8dfc47545d
Andrea Buzzi <aby.development@outlook.it>
2023-07-13 20:07:01 +0200

    Improved Comments

--- Assets/Scripts/CowdexPage.cs
+++ Assets/Scripts/CowdexPage.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CowdexPage : MonoBehaviour
 {
-    //FORECASTED THIS CLASS FOR FUTURE USAGES.
+    //FORECASTED THIS CLASS FOR FUTURE USAGES - EXPECTED TO BE A MONOBEHAVIOUR TO ATTACH TO A GameObject TO HANDLE UI-USED DATA
 
     //DATA
     private ScriptableCow.UniqueID cowUID;
