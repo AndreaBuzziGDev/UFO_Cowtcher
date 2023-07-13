1b9424877dbfd59a8486a254d229014768c6ce86
Andrea Buzzi <aby.development@outlook.it>
2023-07-13 20:05:25 +0200

    CowdexPage script

---
+++ Assets/Scripts/CowdexPage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowdexPage : MonoBehaviour
+{
+    //FORECASTED THIS CLASS FOR FUTURE USAGES.
+
+    //DATA
+    private ScriptableCow.UniqueID cowUID;
+    public ScriptableCow.UniqueID CowUID { get { return cowUID; } }
+
+
+
+    //METHODS
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
