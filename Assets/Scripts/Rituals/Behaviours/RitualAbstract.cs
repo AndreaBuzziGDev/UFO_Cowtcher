0c91fdfef5d2b47a09abfe44811f2e58b8f37cf8
Andrea Buzzi <aby.development@outlook.it>
2023-07-18 18:57:44 +0200

    Perfected refactor of code - discarded useless data

--- Assets/Scripts/Rituals/Behaviours/RitualAbstract.cs
+++ Assets/Scripts/Rituals/Behaviours/RitualAbstract.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public abstract class RitualAbstract
 {
-    //TODO: IMPLEMENT METHOD THAT CHECKS WETHER THIS RITUAL SHOULD "BE DONE" OR NO
-
-    //ENUMS
-    //TODO: FOR NOW IGNORE RITUAL TYPE
-    public enum ERitualType
-    {
-        SimpleCapture,
-        SequentialCapture,
-        ItemProximity,
-        ScoreThreshold
-    }
-
     //DATA
 
 
@@ -23,5 +11,8 @@ public abstract class RitualAbstract
 
     //METHODS
     public abstract void DoRitual();
+
+    //TODO: IMPLEMENT METHOD THAT CHECKS WETHER THIS RITUAL SHOULD "BE DONE" OR NO
+
 
 }
--- Assets/Scripts/Rituals/Behaviours/ScriptableObjects/RitualAbstractSO.cs
+++ Assets/Scripts/Rituals/Behaviours/ScriptableObjects/RitualAbstractSO.cs
@@ -8,21 +8,6 @@ public abstract class RitualAbstractSO : ScriptableObject
     [SerializeField] private List<ScriptableCow.UniqueID> requiredCows = new List<ScriptableCow.UniqueID>();
     public List<ScriptableCow.UniqueID> RequiredCows { get { return requiredCows; } }
 
-    //TODO: IGNORE RITUAL TYPES (WILL NOT BE USED
-    [SerializeField] private ERitualType type = 0;
-    public ERitualType Type { get { return type; } }
-
-    //TODO: POLISH - UNNEEDED
-    public enum ERitualType
-    {
-        SimpleCapture,
-        SequentialCapture,
-        ItemProximity,
-        ScoreThreshold
-    }
-
-
-
 
     //METHODS
     public abstract RitualAbstract GetRitual();
