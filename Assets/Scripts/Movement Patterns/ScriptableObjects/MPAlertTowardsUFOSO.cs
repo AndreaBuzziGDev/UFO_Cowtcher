2750c2bb0263cc414429f733aab93d4346416b01
Andrea Buzzi <aby.development@outlook.it>
2023-07-19 12:11:02 +0200

    Added Decorators£

--- Assets/Scripts/Movement Patterns/ScriptableObjects/MPAlertTowardsUFOSO.cs
+++ Assets/Scripts/Movement Patterns/ScriptableObjects/MPAlertTowardsUFOSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Alert Towards UFO", menuName = "MovementPattern/Alert/Towards UFO")]
 public class MPAlertTowardsUFOSO : MPAbstractAlertSO
 {
     public override AbstractMovementPattern GetMovPattern()
--- Assets/Scripts/Movement Patterns/ScriptableObjects/MPCalmStillnessSO.cs
+++ Assets/Scripts/Movement Patterns/ScriptableObjects/MPCalmStillnessSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Calm Stillness", menuName = "MovementPattern/Calm/Stillness")]
 public class MPCalmStillnessSO : MPAbstractCalmSO
 {
     public override AbstractMovementPattern GetMovPattern()
