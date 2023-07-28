1536b0583e294332e704531ded9c97943af1e65a
iron-gabri97 <ferro197@hotmail.it>
2023-07-29 00:09:42 +0200

    Added zoom-in operation on Follow Camera script and created Game Object VisualChildCamera for zoom management

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -8,6 +8,9 @@ public class FollowCamera : MonoBehaviour
     public Vector3 CameraOffset;
     public float UFOVerticalOffset;
     public float Damping = 1.0f;
+    public Transform parentObject;
+    public float zoomLevel;
+    public float maxZoom;
 
     private void FixedUpdate()
     {
@@ -19,5 +22,14 @@ public class FollowCamera : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, TargetUFO.position + CameraOffset, Time.deltaTime * Damping);
 
         transform.LookAt(TargetUFO.position - new Vector3(0, UFOVerticalOffset, 0), Vector3.up);
+
+        //sets zoom in with float value
+
+        //if()
+        //{
+            transform.position = parentObject.position + (transform.forward * zoomLevel);
+            zoomLevel = Mathf.Clamp(zoomLevel, 0, maxZoom);
+        //}
+
     }
 }
