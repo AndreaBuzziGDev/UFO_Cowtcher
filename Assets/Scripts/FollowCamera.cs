62eb0d8991bce7173c2be2c126e4975772f06271
Andrea Buzzi <aby.development@outlook.it>
2023-07-29 15:51:21 +0200

    Implementing Zoom in a fov-based way

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -8,9 +8,25 @@ public class FollowCamera : MonoBehaviour
     public Vector3 CameraOffset;
     public float UFOVerticalOffset;
     public float Damping = 1.0f;
+
+
     public Transform parentObject;
     public float zoomLevel;
     public float maxZoom;
+
+    //HANDLING ZOOM
+    [SerializeField] private float ZoomMultiplier = 2;
+    private float baseFOV;
+    private Camera cameraComp;
+
+
+
+    private void Awake()
+    {
+        cameraComp = gameObject.GetComponent<Camera>();
+        baseFOV = cameraComp.fieldOfView;
+    }
+
 
     private void FixedUpdate()
     {
@@ -24,12 +40,27 @@ public class FollowCamera : MonoBehaviour
         transform.LookAt(TargetUFO.position - new Vector3(0, UFOVerticalOffset, 0), Vector3.up);
 
         //sets zoom in with float value
-
+        /*
         //if()
         //{
             transform.position = parentObject.position + (transform.forward * zoomLevel);
             zoomLevel = Mathf.Clamp(zoomLevel, 0, maxZoom);
         //}
-
+        */
     }
+
+    public void HandleZoom(bool zooming)
+    {
+        if (zooming)
+        {
+            cameraComp.fieldOfView = baseFOV / ZoomMultiplier;
+        }
+        else
+        {
+            cameraComp.fieldOfView = baseFOV;
+        }
+    }
+
+
+
 }
