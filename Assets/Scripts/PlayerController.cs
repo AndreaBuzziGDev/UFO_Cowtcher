b194f3eec73c70bbb1fe028ea9e34936a0c25e65
Simone-Ciaccio <cimocs@hotmail.it>
2023-07-30 18:40:33 +0200

    Joystick can appear anywhere on the screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -97,11 +97,11 @@ public class PlayerController : MonoBehaviour
     //JOYSTICK
     private void OnScreenTouched(InputAction.CallbackContext value)
     {
-        Vector2 touchPosition = Input.Player.TouchPosition.ReadValue<Vector2>();
-        if (touchPosition.y <= Screen.height / 2)
-        {
+        Vector2 touchPosition = inputPlayer.Player.TouchPosition.ReadValue<Vector2>();
+        //if (touchPosition.y <= Screen.height / 2)
+        //{
             UIController.Instance.ShowJoystick(touchPosition);
-        }
+        //}
     }
 
     private void OnScreenReleased(InputAction.CallbackContext value) => UIController.Instance.HideJoystick();
