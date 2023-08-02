33bc6e20fdfc3429d4818ae87b2d43f38c54786e
Andrea Buzzi <aby.development@outlook.it>
2023-08-02 20:02:40 +0200

    WIP on interactibles

---
+++ Assets/Scripts/Interactibles/Structures/StructureSOAbstract.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StructureSOAbstract : ScriptableObject
+{
+    //THIS WILL BE USED TO REPLICATE THE PATTERN USED ELSEWHERE
+
+
+
+
+}
