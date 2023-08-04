8db86ab9b900f6a491b1b33601d29633876c1392
Andrea Buzzi <aby.development@outlook.it>
2023-08-04 22:20:12 +0200

    WIP on Asteroid stuff, created prefab for asteroids

--- Assets/Scripts/CollisionAsteroids.cs
+++ Assets/Scripts/CollisionAsteroids.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class CollisionAsteroids : MonoBehaviour
 {
-    
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("CollisionAsteroids");
 
-
+        /*
+        GameObject otherGO = collision.gameObject;
+        Cow compCow = otherGO.GetComponent<Cow>();
+        if (compCow != null && (compCow.Rarity == CowSO.Rarity.Legendary))
+        {
+            Debug.Log("compCow.IsPanicking: " + compCow.IsPanicking);
+            if (compCow.IsPanicking)
+            {
+                compCow.Flee();
+            }
+        }
+        */
+    }
 }
