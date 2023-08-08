ba2bd417d11fe60f37352ac58f68e6834afef145
Andrea Buzzi <aby.development@outlook.it>
2023-08-08 19:48:16 +0200

    WIP implementing Scene Navigation in a re-usable way

--- Assets/Scripts/Main Controllers/SceneNavigationController.cs
+++ Assets/Scripts/Main Controllers/SceneNavigationController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneNavigationController : MonoSingleton<SceneNavigationController>
 {
@@ -23,7 +24,14 @@ public class SceneNavigationController : MonoSingleton<SceneNavigationController
 
 
     //DATA
-    public List<Sprite> SceneSprites = new();
+    [SerializeField] private List<Sprite> SceneSprites = new();
+    [SerializeField] private List<Scene> TechnicalScenes = new();
+    [SerializeField] private List<Scene> StageScenes = new();
+
+    //
+    private Dictionary<string, Scene> TechnicalSceneDictionary = new();
+    private Dictionary<string, Scene> StageSceneDictionary = new();
+    private Dictionary<string, Sprite> SceneSpritesDictionary = new();//TODO: IMPLEMENT DATA STRUCTURE TO EASILY USE THIS
 
 
 
@@ -33,7 +41,18 @@ public class SceneNavigationController : MonoSingleton<SceneNavigationController
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (Scene s in TechnicalScenes)
+        {
+            TechnicalSceneDictionary.Add(s.name, s);
+        }
+
+        foreach (Scene s in StageScenes)
+        {
+            StageSceneDictionary.Add(s.name, s);
+        }
+
+        //SCENE SPRITES? (MIGHT NOT BE NECESSARY WITH MORE COMPLEX IMPLEMENTATION
+
     }
 
     // Update is called once per frame
@@ -43,6 +62,92 @@ public class SceneNavigationController : MonoSingleton<SceneNavigationController
     }
 
 
+
+
+
     //FUNCTIONALITIES
+
+    //SCENE HANDLING
+
+    ///TECHNICAL SCENES
+    public void LoadScene(eTechnicalSceneName targetScene)
+    {
+        string intendedScene = "";
+        switch (targetScene)
+        {
+            case eTechnicalSceneName.MainMenu:
+                intendedScene = "";//TODO: 
+                break;
+            case eTechnicalSceneName.Opening:
+                intendedScene = "";//TODO: 
+                break;
+            case eTechnicalSceneName.Credits:
+                intendedScene = "";//TODO: 
+                break;
+        }
+
+        //TODO: THIS CAN BE EXPORTED AS A DEDICATED FUNCTIONALITY
+        if (!string.IsNullOrEmpty(intendedScene)) SceneManager.LoadScene(intendedScene);
+        else Debug.Log("Invalid Target Scene: " + targetScene);
+
+    }
+
+
+    ///STAGE SCENES
+    public Sprite GetAssociatedSprite(eStageSceneName targetScene)
+    {
+        Sprite intendedSprite;
+
+        switch (targetScene)
+        {
+            //TODO: USE A COMPLEX DATA STRUCTURE (SCRIPTABLEOBJECTS OR PREFABS) TO HANDLE THIS IN A BETTER WAY
+
+            case eStageSceneName.Stage1:
+                intendedSprite = SceneSprites[1];
+                break;
+            case eStageSceneName.Stage2:
+                intendedSprite = SceneSprites[2];
+                break;
+            case eStageSceneName.Stage3:
+                intendedSprite = SceneSprites[3];
+                break;
+            case eStageSceneName.Stage4:
+                intendedSprite = SceneSprites[4];
+                break;
+            default:
+                intendedSprite = SceneSprites[0];
+                break;
+        }
+
+        return intendedSprite;
+    }
+
+    public void LoadScene(eStageSceneName targetScene)
+    {
+        string intendedScene;
+        switch (targetScene)
+        {
+            case eStageSceneName.Stage1:
+                intendedScene = "";//TODO: 
+                break;
+            case eStageSceneName.Stage2:
+                intendedScene = "";//TODO: 
+                break;
+            case eStageSceneName.Stage3:
+                intendedScene = "";//TODO: 
+                break;
+            case eStageSceneName.Stage4:
+                intendedScene = "";//TODO: 
+                break;
+            default:
+                intendedScene = "";
+                break;
+        }
+
+        //TODO: THIS CAN BE EXPORTED AS A DEDICATED FUNCTIONALITY
+        if (!string.IsNullOrEmpty(intendedScene)) SceneManager.LoadScene(intendedScene);
+        else Debug.Log("Invalid Target Scene: " + targetScene);
+
+    }
 
 }
