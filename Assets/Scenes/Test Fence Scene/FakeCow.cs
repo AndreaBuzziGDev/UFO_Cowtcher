7735fb1e0dde5cfcc8263aa185c3a38b077ad26a
Andrea Buzzi <aby.development@outlook.it>
2023-08-10 14:31:52 +0200

    WIP implementing feature

--- Assets/Scenes/Test Fence Scene/FakeCow.cs
+++ Assets/Scenes/Test Fence Scene/FakeCow.cs
@@ -7,17 +7,33 @@ public class FakeCow : MonoBehaviour
     //DATA
 
     [SerializeField] private FakeUFO myUFO;
+    [SerializeField] private float mySpeed = 5.0f;
+
+    private Rigidbody myRB;
+
+
+    //DIRECTION
+    private Vector3 myDirection;
+    
+    //ROTATION FOR THAT DIRECTION
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 ufoPos = myUFO.transform.position;
+        Vector3 ufoProjectedPos = new Vector3(ufoPos.x, 0, ufoPos.z);
+
+        myDirection = this.transform.position - ufoProjectedPos;
+
+        myRB = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        myRB.velocity = mySpeed * myDirection;
     }
 }
