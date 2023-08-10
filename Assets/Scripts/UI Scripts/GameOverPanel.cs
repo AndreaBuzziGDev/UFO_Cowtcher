d9d1e9a1923234958e4481f84111e31f2db73320
Andrea Buzzi <aby.development@outlook.it>
2023-08-10 03:03:37 +0200

    WIP implementing stuff related to the GUI Juicyness

--- Assets/Scripts/UI Scripts/GameOverPanel.cs
+++ Assets/Scripts/UI Scripts/GameOverPanel.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class GameOverPanel : MonoBehaviour
 {
+    //DATA
+
+    ///GUI REFERENCES
+    [SerializeField] private QuitSubPanel QuitSubPanel;
+
+
+    //METHODS
+    private void Start()
+    {
+        QuitSubPanel.gameObject.SetActive(false);
+    }
+
+
+    private void OnDisable()
+    {
+        QuitSubPanel.gameObject.SetActive(false);
+    }
+
+
     //UNPAUSE
     public void Restart() => GameController.Instance.RestartScene();
 
     //QUIT
-    public void Quit() => GameController.Instance.SetState(GameController.EGameState.Quitting);
+    public void Quit()
+    {
+        //
+        QuitSubPanel.gameObject.SetActive(true);
+    }
 }
