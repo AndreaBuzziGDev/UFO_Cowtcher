4b1bd9cb7f987bf3d412437790793b3a0d7197d9
Andrea Buzzi <aby.development@outlook.it>
2023-08-10 14:27:40 +0200

    Investigating feature

--- Assets/Scenes/Test Fence Scene/Fence.cs
+++ Assets/Scenes/Test Fence Scene/Fence.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FakeUFO : MonoBehaviour
+public class Fence : MonoBehaviour
 {
-
-
     // Start is called before the first frame update
     void Start()
     {
