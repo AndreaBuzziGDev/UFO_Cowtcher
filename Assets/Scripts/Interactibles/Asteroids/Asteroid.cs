e24bdd7634b324a0d3102eb66a9d7a9b6a13f0df
Andrea Buzzi <aby.development@outlook.it>
2023-08-13 01:24:01 +0200

    Implementing Juicyness and other properties, leaving some of the juicyness stuff for later (unnecessary at the moment

--- Assets/Scripts/Interactibles/Asteroids/Asteroid.cs
+++ Assets/Scripts/Interactibles/Asteroids/Asteroid.cs
@@ -5,12 +5,36 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     //DATA
+    ///JUICYNESS - ASTEROID DIRECTION AND SPEED PROPERTIES
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float angleX = 10;
+    [SerializeField] private float angleZ = 10;
+
+
+    ///GAMEPLAY PROPERTIES
+    [SerializeField] [Range(1f, 100f)] private float contentSpawnChance = 30;
+    [SerializeField] private bool grantAtLeastOne;
+
+
+    ///ASTEROID CONTENT
     [SerializeField] private MonoInteractible myAsteroidContent;
 
 
 
 
     //METHODS
+    //...
+    private void Awake()
+    {
+        if (myAsteroidContent == null) Debug.LogError("Asteroid " + this.gameObject.name + " has no content assigned.");
+    }
+
+    private void Start()
+    {
+
+
+    }
+
 
     //COLLISION
     void OnCollisionEnter(Collision collision)
