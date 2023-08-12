8598cfd24cbcaab560d741b23bf415b4584ef3fe
Andrea Buzzi <aby.development@outlook.it>
2023-08-12 23:13:08 +0200

    Removing Excess Debug

--- Assets/Scripts/Player Scripts/UFOFuelHelper.cs
+++ Assets/Scripts/Player Scripts/UFOFuelHelper.cs
@@ -26,19 +26,9 @@ public class UFOFuelHelper
     //METHODS
     public static int FractionMode(CowSO cowTemplate)
     {
-
-        Debug.Log("UFO - Cow " + cowTemplate.UID + " Already Captured : " + DifficultyManager.Instance.GetCountCapturedCow(cowTemplate.UID));
-
         float factoredFuel = ((float)cowTemplate.FuelRecoveryAmount) / (1 + DifficultyManager.Instance.GetCountCapturedCow(cowTemplate.UID));
-        Debug.Log("UFO - factoredFuel: " + factoredFuel);
-
         float ceiledFuel = Mathf.Ceil(factoredFuel);
-        Debug.Log("UFO - ceiledFuel: " + ceiledFuel);
-
-        int intFuel = (int)ceiledFuel;
-        Debug.Log("UFO - intFuel: " + intFuel);
-
-        return intFuel;
+        return (int)ceiledFuel;
     }
 
 
