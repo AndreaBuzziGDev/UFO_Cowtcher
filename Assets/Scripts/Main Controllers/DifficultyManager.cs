ed602cafe3c7d5a78ebfcca6e726fe253cb042a7
Andrea Buzzi <aby.development@outlook.it>
2023-08-18 01:59:31 +0200

    Minor change

--- Assets/Scripts/Main Controllers/DifficultyManager.cs
+++ Assets/Scripts/Main Controllers/DifficultyManager.cs
@@ -35,19 +35,6 @@ public class DifficultyManager : MonoSingleton<DifficultyManager>
     //...
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
     //INITIALIZATION
     public void Initialization()
     {
