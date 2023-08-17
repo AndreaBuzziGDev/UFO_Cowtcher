155b82b83e72e06016d5df0f7b820f7dd529ce73
Andrea Buzzi <aby.development@outlook.it>
2023-08-18 01:54:35 +0200

    Creating MoossionManager

---
+++ Assets/Scripts/Main Controllers/MoossionManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoossionManager : MonoSingleton<MoossionManager>
+{
+    //DATA
+
+
+
+    //METHODS
+    //...
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //FUNCTIONALITIES
+
+
+
+
+
+}
