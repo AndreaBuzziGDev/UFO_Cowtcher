8c584b1df7d3a49e79931c3d2cd09c113e7c7651
Andrea Buzzi <aby.development@outlook.it>
2023-08-18 02:00:06 +0200

    Removed Start - Using Initialization instead

--- Assets/Scripts/Main Controllers/MoossionManager.cs
+++ Assets/Scripts/Main Controllers/MoossionManager.cs
@@ -13,12 +13,6 @@ public class MoossionManager : MonoSingleton<MoossionManager>
     //METHODS
     //...
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
