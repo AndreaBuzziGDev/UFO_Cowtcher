164edabd502cd3fe4d154b5ec81f4f599c9867a8
Andrea Buzzi <aby.development@outlook.it>
2023-08-19 20:13:21 +0200

    Preparing dedicated controller for spawning cows

---
+++ Assets/Scripts/Main Controllers/SpawnManagerCow.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerCow : MonoSingleton<SpawnManagerCow>
+{
+    //DATA
+
+
+
+    //METHODS
+    //...
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    //INITIALIZATION
+
+
+
+    //FUNCTIONALITY
+
+
+
+    //UTILITY
+
+
+}
