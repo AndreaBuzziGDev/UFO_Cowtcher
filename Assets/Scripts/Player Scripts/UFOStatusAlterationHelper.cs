27d7834417c722976b703f4938208ee661a7e2ee
Andrea Buzzi <aby.development@outlook.it>
2023-08-21 01:46:47 +0200

    Implementing an helper that serves to handle some logic related to the Buffs of UFOs

---
+++ Assets/Scripts/Player Scripts/UFOStatusAlterationHelper.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOStatusAlterationHelper
+{
+    //ALL STATIC CODE
+
+    //
+    public static bool HasBuffMoveSpeed()
+    {
+
+
+        return false;
+    }
+
+    public static bool HasBuffCaptureSpeed()
+    {
+
+        return false;
+    }
+
+    public static bool HasBuffFuelGain()
+    {
+
+        return false;
+    }
+
+    public static bool HasBuffRadar()
+    {
+
+        return false;
+    }
+
+    public static bool HasBuffCaptureRadius()
+    {
+
+        return false;
+    }
+}
