7f6f53f264d346816aba392c10141178185faa61
Andrea Buzzi <aby.development@outlook.it>
2023-08-21 17:11:43 +0200

    Implementing Test save script

---
+++ Assets/Scripts/Save System/SaveTestScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveTestScript : MonoBehaviour
+{
+    //DATA
+    CowSO.UniqueID testCow = CowSO.UniqueID.C000Jamal;
+
+
+    //METHODS
+    //...
+
+    //TEST SAVE COW
+    public void TestSaveCow()
+    {
+
+
+    }
+
+
+    //TEST LOAD COW
+    public void TestLoadCow()
+    {
+        SaveInfoCow cowSI = SaveSystem.LoadCowProgress(testCow);
+        Debug.Log("cowSI CowUID" + cowSI.CowUID);
+        Debug.Log("cowSI KnowledgeValue" + cowSI.KnowledgeValue);
+        Debug.Log("cowSI IsKnown" + cowSI.IsKnown);
+        Debug.Log("cowSI IsCaptured" + cowSI.IsCaptured);
+
+    }
+
+
+    //TEST ...
+
+
+}
