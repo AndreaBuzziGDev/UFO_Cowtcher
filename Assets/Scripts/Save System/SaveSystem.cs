54cdf811298f79fa569e152c420e1910209ab9b1
Andrea Buzzi <aby.development@outlook.it>
2023-08-21 15:29:13 +0200

    WIP on save system

---
+++ Assets/Scripts/Save System/SaveSystem.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSystem
+{
+    //SAVE SYSTEM IS DRIVEN BY STATIC CODE
+
+    //DATA
+    public const string HSAddress = "HighScore";
+    public const string StageAddress = "Stages/";
+
+
+
+    //MOOSSION PROGRESS INFO
+    public static void SaveMoossion()
+    {
+
+    }
+
+    //SAVE STAGE UNLOCKS
+    public static void SaveStage(string stageName, int knowledgeValue)
+    {
+        PlayerPrefs.SetInt(StageAddress + stageName, knowledgeValue);//0 - STAGE LOCKED, ANY ABOVE = UNLOCKED
+        PlayerPrefs.Save();
+    }
+
+    public static bool LoadStage(string stageName)
+    {
+        return PlayerPrefs.GetInt(StageAddress + stageName, 0) > 0;//0 - STAGE LOCKED, ANY ABOVE = UNLOCKED
+    }
+
+    public static void ResetStages()
+    {
+        PlayerPrefs.SetInt(StageAddress + "Stage 1", 0);
+        PlayerPrefs.SetInt(StageAddress + "Stage 2", 0);
+        PlayerPrefs.SetInt(StageAddress + "Stage 3", 0);
+        PlayerPrefs.SetInt(StageAddress + "Stage 4", 0);
+        PlayerPrefs.Save();
+    }
+
+
+
+
+    //SAVE HIGH SCORE
+    public static void SaveHighScore(int inputHighScore)
+    {
+        int currentHighScore = LoadHighScore();
+        if (currentHighScore >= inputHighScore)
+        {
+            Debug.Log("SaveSystem - SaveHighScore - input score is not greater than current high score.");
+        }
+        else
+        {
+            PlayerPrefs.SetInt(HSAddress, inputHighScore);
+            PlayerPrefs.Save();
+        }
+    }
+
+    public static int LoadHighScore()
+    {
+        return PlayerPrefs.GetInt(HSAddress, 0);
+    }
+
+    public static void ResetHighScore()
+    {
+        Debug.LogError("SaveSystem - RESETTING HIGH SCORE.");
+        PlayerPrefs.SetInt(HSAddress, 0);
+        PlayerPrefs.Save();
+    }
+
+
+
+
+    //COW PROGRESS INFO
+    public static void SaveCowProgress()
+    {
+
+    }
+
+
+
+}
