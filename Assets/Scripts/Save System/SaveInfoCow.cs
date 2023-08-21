4aaee508c9b5268b0d090b9f4655ce580461c553
Andrea Buzzi <aby.development@outlook.it>
2023-08-21 16:02:56 +0200

    Implementing Cow Save Info

--- Assets/Scripts/Save System/SaveInfoCow.cs
+++ Assets/Scripts/Save System/SaveInfoCow.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SaveInfoCow : MonoBehaviour
+public class SaveInfoCow
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //ENUMS
+    public enum Knowledge{
+        Unknown = 0,
+        Known = 1,
+        Captured = 2
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //DATA
+    private CowSO.UniqueID savedCowUID;
+    private Knowledge knowledgeValue;
+
+    ///DATA GETTERS
+    public CowSO.UniqueID CowUID { get { return savedCowUID; } }
+
+    public Knowledge KnowledgeValue { get { return knowledgeValue; } }
+
+    public bool IsKnown { get { return 1 == (int)knowledgeValue; } }
+    public bool IsCaptured { get { return 2 == (int)knowledgeValue; } }
+
+
+
+    //CONSTRUCTOR
+    private SaveInfoCow()
     {
-        
+        //PRIVATE EMPTY CONSTRUCTOR TO AVOID INSTANCIATION OF AN EMPTY DATA CLASS
+
     }
+
+    public SaveInfoCow(CowSO.UniqueID cowUID, Knowledge knowValue)
+    {
+        savedCowUID = cowUID;
+        knowledgeValue = knowValue;
+    }
+
+    public SaveInfoCow(int cowIndex, int knowValue)
+    {
+        savedCowUID = (CowSO.UniqueID) cowIndex;
+        knowledgeValue = (Knowledge) knowValue;
+
+    }
+
+
+
 }
