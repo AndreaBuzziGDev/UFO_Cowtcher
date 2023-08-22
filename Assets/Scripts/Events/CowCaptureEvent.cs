29bf4a51101cf9522b9619eb18f171e9e86ba18d
Andrea Buzzi <aby.development@outlook.it>
2023-08-22 12:02:19 +0200

    Implementing Event system, but discarded for now

---
+++ Assets/Scripts/Events/CowCaptureEvent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowCaptureEvent 
+{
+    //TODO: USE THIS AS A VARIANT FOR THE ABDUCTOR SYSTEM
+
+
+}
