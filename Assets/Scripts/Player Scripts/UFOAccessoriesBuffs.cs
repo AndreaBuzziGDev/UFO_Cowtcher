96c74d67624c7d3c54da4ab56e0726b08589b969
iron-gabri97 <ferro197@hotmail.it>
2023-08-23 01:04:53 +0200

    Implemented some visible buffs on the UFO when activating

--- Assets/Scripts/Player Scripts/UFOAccessoriesBuffs.cs
+++ Assets/Scripts/Player Scripts/UFOAccessoriesBuffs.cs
@@ -1,18 +1,43 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UFOAccessoriesBuffs : MonoBehaviour
+
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //DATA
+    [SerializeField] private SpriteRenderer speedBoostUFO;
+    [SerializeField] private SpriteRenderer fastCatchUFO;
+    [SerializeField] private SpriteRenderer fuelBoostUFO;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //BUFF SPEED BOOST
+        if (UFOStatusAlterationHelper.HasBuffMoveSpeed() == true)
+        {
+            speedBoostUFO.gameObject.SetActive(true);
+        }
+        else
+            speedBoostUFO.gameObject.SetActive(false);
+
+        //BUFF FAST CATCH
+        if (UFOStatusAlterationHelper.HasBuffCaptureSpeed() == true)
+        {
+            fastCatchUFO.gameObject.SetActive(true);
+        }
+        else
+            fastCatchUFO.gameObject.SetActive(false);
+
+        //BUFF FUEL BOOST
+        if (UFOStatusAlterationHelper.HasBuffFuelGain() == true)
+        {
+            fuelBoostUFO.gameObject.SetActive(true);
+        }
+        else
+            fuelBoostUFO.gameObject.SetActive(false); 
     }
 }
