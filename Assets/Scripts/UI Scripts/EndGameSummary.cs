938b1b3e4bc7f102970cfd94c6f036b2718ab6d1
Andrea Buzzi <aby.development@outlook.it>
2023-08-24 23:10:04 +0200

    Implementing Final Gamescore

--- Assets/Scripts/UI Scripts/EndGameSummary.cs
+++ Assets/Scripts/UI Scripts/EndGameSummary.cs
@@ -1,9 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndGameSummary : MonoBehaviour
 {
+    //DATA
+
+    ///GUI REFERENCES
+    [SerializeField] private TMPro.TextMeshProUGUI highscoreValue;
+
+    [SerializeField] private TMPro.TextMeshProUGUI capturesValue;
+    [SerializeField] private TMPro.TextMeshProUGUI timeElapsedValue;
+
+    [SerializeField] private TMPro.TextMeshProUGUI moossionOne;
+    [SerializeField] private TMPro.TextMeshProUGUI moossionTwo;
+    [SerializeField] private TMPro.TextMeshProUGUI moossionThree;
+
+
+    [SerializeField] private TMPro.TextMeshProUGUI multiplierValue;
+    [SerializeField] private TMPro.TextMeshProUGUI actualExpValue;
+
+    [SerializeField] private StageExpBar expBar;
+
+
+    //METHODS
+    //...
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +38,9 @@ public class EndGameSummary : MonoBehaviour
     {
         
     }
+
+    //FUNCTIONALITIES
+
+
+
 }
