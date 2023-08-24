d0f1ce6ff2950eccc3bb67f218bfb352868e917e
iron-gabri97 <ferro197@hotmail.it>
2023-08-24 23:11:57 +0200

    Deleted the contents of the Music Controller script and created the prefab of the game object Music Controller

--- Assets/Scripts/Main Controllers/MusicController.cs
+++ Assets/Scripts/Main Controllers/MusicController.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class MusicController : MonoBehaviour
 {
-    AudioSource[] musicSources;
-
-    private void Awake()
-    {
-        musicSources = GetComponents<AudioSource>();
-    }
-
-    public void Play (AudioClip _clip)
-    {
-        for(int i = 0; i < musicSources.Length; i++)
-        {
-            if (!musicSources[i].isPlaying)
-            {
-                musicSources[i].clip = _clip;
-                musicSources[i].Play();
-                break;
-            }
-        }
-    }
+    
 }
