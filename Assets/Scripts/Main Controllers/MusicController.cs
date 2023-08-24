7a77e6761670a45ce42064291e7d4bae3cb9c527
iron-gabri97 <ferro197@hotmail.it>
2023-08-24 22:33:33 +0200

    Created Music Controller script

---
+++ Assets/Scripts/Main Controllers/MusicController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    AudioSource[] musicSources;
+
+    private void Awake()
+    {
+        musicSources = GetComponents<AudioSource>();
+    }
+
+    public void Play (AudioClip _clip)
+    {
+        for(int i = 0; i < musicSources.Length; i++)
+        {
+            if (!musicSources[i].isPlaying)
+            {
+                musicSources[i].clip = _clip;
+                musicSources[i].Play();
+            }
+        }
+    }
+}
