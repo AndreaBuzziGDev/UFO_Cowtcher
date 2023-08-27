99d99b22a404790e30105b39fba9ffa394cd4c33
Andrea Buzzi <aby.development@outlook.it>
2023-08-27 23:10:29 +0200

    Fixed code so that the existing Audio Source is used instead of creating a new component

--- Assets/Scripts/Main Controllers/MusicController.cs
+++ Assets/Scripts/Main Controllers/MusicController.cs
@@ -26,12 +26,6 @@ public class MusicController : MonoSingleton<MusicController>
     //TODO: WHEN MAKING MUSIC PLAY BASED ON THE SCENE IT IS RUNNING IN, SWITCH TO AN "INITIALIZE" SOLUTION INSTEAD OF OnEnable
     private void OnEnable()
     {
-        activeMusicSource = gameObject.AddComponent<AudioSource>();
-        activeMusicSource.loop = true;
-
-        moossionCompleteSource = gameObject.AddComponent<AudioSource>();
-        activeMusicSource.loop = false;
-
         //MAKE THE INTENDED MUSIC PLAY
         //TODO: SELECT MUSIC CORRECTLY BASED ON THE ACTIVE SCENE
         PlayMusic(gameplayMusic, activeMusicSource);
