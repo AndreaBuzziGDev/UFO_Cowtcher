e043623fbe742754885c05e7991766cdc01406ed
Andrea Buzzi <aby.development@outlook.it>
2023-08-29 03:01:03 +0200

    Implementing Capture Radius, WIP

---
+++ Assets/Scripts/Status Alterations/SACaptureRadius.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SACaptureRadius : SAAbstract
+{
+    //DATA
+    ///USEFUL BUFF DATA
+    public float buffDuration;
+    public float captureRadiusIncrease;
+    Abductor abductorScriptReference;
+
+    ///TEMPLATE
+    SACaptureRadiusSO template;
+
+
+
+    //CONSTRUCTOR
+    public SACaptureRadius(SACaptureRadiusSO inputTemplate)
+    {
+        this.template = inputTemplate;
+        this.type = template.buffType;
+        this.buffDuration = template.buffDuration;
+        this.captureRadiusIncrease = template.captureRadiusIncrease;
+        abductorScriptReference = GameController.Instance.FindAbductorAnywhere();
+    }
+
+    //METHODS
+    ///TEMPLATE
+    public override SAAbstractSO Template() => template;
+
+    ///BUFF
+    public override void ApplyBuff()
+    {
+        /*
+        abductorScriptReference.SetCaptureSpeedBoost(this.captureSpeedIntensity);
+        UIController.Instance.IGPanel.BuffPanel.ActivateSpeedBoost();
+        */
+    }
+
+    public override void ExpireBuff()
+    {
+        /*
+        abductorScriptReference.SetCaptureSpeedBoost(0);
+        */
+    }
+}
