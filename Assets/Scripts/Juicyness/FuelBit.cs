296ac13ab9f18dd70bee1e61b023ced86abd65fb
Simone-Ciaccio <cimocs@hotmail.it>
2023-09-10 00:32:02 +0200

    Added comments

--- Assets/Scripts/Juicyness/FuelBit.cs
+++ Assets/Scripts/Juicyness/FuelBit.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class FuelBit : MonoBehaviour
 {
+    // if this entire script works, we only need to spawn it (maybe from the cow to easily use its fuelAmount as spawn number)
+    // maybe with a very small delay between each spawn
+
     private Camera cam;
-    [SerializeField] private float speed;
+    [SerializeField] private float speed; // set to 0.1 in inspector just for testing, change later
 
     private Vector3 startingPosition;
 
@@ -25,7 +28,8 @@ public class FuelBit : MonoBehaviour
 
     void Update()
     {
-        fuelBitTargetLocation = cam.ScreenToWorldPoint(fuelBitTarget.pivot);
+        // wanted to get the world position of the fuelBitTarget set in the IGPanel relative to the screen but I can't find a way to do it
+        fuelBitTargetLocation = cam.ScreenToWorldPoint(fuelBitTarget.transform.position);
         fuelBitTargetLocation = new Vector3(fuelBitTargetLocation.x, fuelBitTargetLocation.y, GameController.Instance.FindUFOAnywhere().gameObject.transform.position.z);
 
         interpolationTime += Time.deltaTime;
@@ -36,6 +40,7 @@ public class FuelBit : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        // this is just to be able to see where the fuelBit goes towards
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(fuelBitTargetLocation, 1);
     }
