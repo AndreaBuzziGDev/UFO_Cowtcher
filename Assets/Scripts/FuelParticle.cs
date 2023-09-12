6b6ea816d8ea0f414a70d1158eaf1acc474e92bb
Andrea Buzzi <aby.development@outlook.it>
2023-09-12 22:43:20 +0200

    Solution not yet working

--- Assets/Scripts/FuelParticle.cs
+++ Assets/Scripts/FuelParticle.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class FuelParticle : MonoBehaviour
 {
     //DATA
+    private Camera cam;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float particleSpeed = 5.0f;
 
-    private GameObject targetObject;
+    private GameObject targetObject;//TODO: TRY RECT TRANSFORM
     private Vector3 destination;
 
 
@@ -16,6 +17,7 @@ public class FuelParticle : MonoBehaviour
     //...
     private void Start()
     {
+        cam = Camera.main;
         targetObject = UIController.Instance.IGPanel.PlayerFuelBar.gameObject;
         destination = targetObject.transform.position;
     }
@@ -23,7 +25,12 @@ public class FuelParticle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         //MOVE TOWARDS FUEL BAR
         rb.velocity = particleSpeed * destination.normalized;
+        */
+        Vector3 fuelBitTargetLocation = cam.WorldToViewportPoint(targetObject.transform.position);
+        rb.velocity = particleSpeed * fuelBitTargetLocation.normalized;
+
     }
 }
