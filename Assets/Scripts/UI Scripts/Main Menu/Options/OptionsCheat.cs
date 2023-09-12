902d85a96d101d75aebc6edc6e2d8074736b9dbc
Andrea Buzzi <aby.development@outlook.it>
2023-09-13 01:42:36 +0200

    Implemented even more cheats for development purposes

--- Assets/Scripts/UI Scripts/Main Menu/Options/OptionsCheat.cs
+++ Assets/Scripts/UI Scripts/Main Menu/Options/OptionsCheat.cs
@@ -26,6 +26,29 @@ public class OptionsCheat : MonoBehaviour
         Debug.Log("DEVELOPER CHEAT - STAGE 1 LEVEL 7");
         Debug.Log("DEVELOPER CHEAT - RESULT: " + SaveSystem.LoadStageLevelInfo(SceneNavigationController.Instance.GetAssociatedName(SceneNavigationController.eStageSceneName.Stage1)));
     }
+    public void CheatThree()
+    {
+        SaveSystem.SetStageLevelInfo(SceneNavigationController.Instance.GetAssociatedName(SceneNavigationController.eStageSceneName.Stage2), 7);
 
+        //DEBUGGING
+        Debug.Log("DEVELOPER CHEAT - STAGE 2 LEVEL 7");
+        Debug.Log("DEVELOPER CHEAT - RESULT: " + SaveSystem.LoadStageLevelInfo(SceneNavigationController.Instance.GetAssociatedName(SceneNavigationController.eStageSceneName.Stage2)));
+    }
+    public void CheatFour()
+    {
+        SaveSystem.SetStageLevelInfo(SceneNavigationController.Instance.GetAssociatedName(SceneNavigationController.eStageSceneName.Stage3), 7);
+
+        //DEBUGGING
+        Debug.Log("DEVELOPER CHEAT - STAGE 3 LEVEL 7");
+        Debug.Log("DEVELOPER CHEAT - RESULT: " + SaveSystem.LoadStageLevelInfo(SceneNavigationController.Instance.GetAssociatedName(SceneNavigationController.eStageSceneName.Stage3)));
+    }
+    public void CheatFive()
+    {
+        SaveSystem.SetStageLevelInfo(SceneNavigationController.Instance.GetAssociatedName(SceneNavigationController.eStageSceneName.Stage4), 7);
+
+        //DEBUGGING
+        Debug.Log("DEVELOPER CHEAT - STAGE 4 LEVEL 7");
+        Debug.Log("DEVELOPER CHEAT - RESULT: " + SaveSystem.LoadStageLevelInfo(SceneNavigationController.Instance.GetAssociatedName(SceneNavigationController.eStageSceneName.Stage4)));
+    }
 
 }
