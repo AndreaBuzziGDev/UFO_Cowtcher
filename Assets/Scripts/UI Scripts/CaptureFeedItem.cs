ed07cc710af3ba992d9af2a06469703f7f43c474
Andrea Buzzi <aby.development@outlook.it>
2023-09-16 19:02:33 +0200

    Moving scripts in the appropriate place, preparing script for feed item when a new cow is captured

--- Assets/Scripts/UI Scripts/CaptureFeedItem.cs
+++ Assets/Scripts/UI Scripts/CaptureFeedItem.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class CaptureFeedItem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //DATA
+    [SerializeField] private float persistenceTimerMax = 5.0f;
+    private float persistenceTimer;
+
+    ///GUI REFERENCES
+    [SerializeField] private TMPro.TextMeshProUGUI feedItemText;
+
+
+
+
+    //METHODS
+    //...
     void Start()
     {
-        
+        //REGISTER EVENT
+        //Moossion.MoossionComplete += HandleMoossionCompletion;
+
+        //TEXT ON START = EMPTY
+        feedItemText.text = "";
+
+        //DISABLE ON GUI
+        this.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (persistenceTimer > 0)
+        {
+            persistenceTimer -= Time.deltaTime;
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
     }
+
+
+    private void OnDestroy()
+    {
+        //UN-REGISTER EVENT
+        //Moossion.MoossionComplete -= HandleMoossionCompletion;
+    }
+
+
+
+
+    //EVENT-HANDLING
+    //TODO: SWITCH TO ANOTHER EVENT
+    private void HandleMoossionCompletion(object sender, MoossionCompleteEventArgs e)
+    {
+        //SHOW MOOSSION FEED ITEM AND ITS CONTENT
+        persistenceTimer = persistenceTimerMax;
+        feedItemText.text = "Captured cow #" + e.MoossionIndex + " complete!";
+        this.gameObject.SetActive(true);
+    }
+
 }
