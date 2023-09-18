457df7920ed009cfe2c5b16da541925d66a81aa8
Andrea Buzzi <aby.development@outlook.it>
2023-09-18 03:13:51 +0200

    Implementing feed

---
+++ Assets/Scripts/UI Scripts/FeedPanelShortcuts.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedPanelShortcuts : MonoBehaviour
+{
+    //FUNCTIONALITIES
+    public void MoossionShortcut()
+    {
+        Debug.Log("ClickableFeedItem - MoossionShortcut");
+    }
+    public void CowdexShortcut()
+    {
+        Debug.Log("ClickableFeedItem - CowdexShortcut");
+    }
+
+}
