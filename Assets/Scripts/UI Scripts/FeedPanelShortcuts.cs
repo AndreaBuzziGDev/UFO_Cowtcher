9c2bc9d3b4f8165272d7f1e6fabaf1a98866020c
Andrea Buzzi <aby.development@outlook.it>
2023-09-18 03:44:14 +0200

    Fixed UI

--- Assets/Scripts/UI Scripts/FeedPanelShortcuts.cs
+++ Assets/Scripts/UI Scripts/FeedPanelShortcuts.cs
@@ -8,14 +8,13 @@ public class FeedPanelShortcuts : MonoBehaviour
     public void MoossionShortcut()
     {
         GameController.Instance.SetState(GameController.EGameState.Paused);
-        UIController.Instance.ShowCowdex();
-
+        UIController.Instance.ShowMoossions();
     }
+
     public void CowdexShortcut()
     {
         GameController.Instance.SetState(GameController.EGameState.Paused);
-        UIController.Instance.ShowMoossions();
-
+        UIController.Instance.ShowCowdex();
     }
 
 }
