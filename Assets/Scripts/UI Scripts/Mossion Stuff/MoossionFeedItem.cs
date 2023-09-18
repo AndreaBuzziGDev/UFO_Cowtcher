f368ce84cb9916666831fe0efc0c2ef9870bf689
Andrea Buzzi <aby.development@outlook.it>
2023-09-18 02:18:08 +0200

    Implementing fix

--- Assets/Scripts/UI Scripts/Mossion Stuff/MoossionFeedItem.cs
+++ Assets/Scripts/UI Scripts/Mossion Stuff/MoossionFeedItem.cs
@@ -12,6 +12,14 @@ public class MoossionFeedItem : MonoBehaviour
     ///GUI REFERENCES
     [SerializeField] private TMPro.TextMeshProUGUI feedItemText;
 
+    //TODO: THIS CODE IS COPIED FROM CaptureFeedItem - IF TIME IS AVAILABLE, USE INHERITANCE TO PROPERLY FACTOR CODE
+    ///GUI ANIMATION STUFF
+    //TODO: THIS COULD BE IMPROVED BY PROGRAMMATICALLY LOOKING FOR THE SIZE OF THE SCREEN
+    [SerializeField] private Vector3 slidingOffset = new Vector3(500, 0, 0);
+    private Vector3 startingPos = Vector3.zero;
+
+    [SerializeField] private float slideInTimerMax = 1.0f;
+    private float slideInTimer;
 
 
 
@@ -22,24 +30,29 @@ public class MoossionFeedItem : MonoBehaviour
         //REGISTER EVENT
         Moossion.MoossionComplete += HandleMoossionCompletion;
 
+        //DEFAULT POSITION
+        startingPos = this.transform.position;
+
         //TEXT ON START = EMPTY
         feedItemText.text = "";
 
         //DISABLE ON GUI
-        this.gameObject.SetActive(false);
+        //TODO: UN-COMMENT
+        //this.gameObject.SetActive(false);
+
+        //TODO: REMOVE
+        persistenceTimer = persistenceTimerMax;
+        slideInTimer = slideInTimerMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (persistenceTimer > 0)
-        {
-            persistenceTimer -= Time.deltaTime;
-        }
-        else
-        {
-            this.gameObject.SetActive(false);
-        }
+        //HANDLE SLIDE-IN
+        HandleSlideIn();
+
+        //HANDLE PERSISTENCE ON SCREEN
+        HandlePersistence();
     }
 
 
@@ -50,7 +63,27 @@ public class MoossionFeedItem : MonoBehaviour
     }
 
 
-    
+    //FUNCTIONALITIES
+
+    ///SLIDE-IN
+    private void HandleSlideIn()
+    {
+        this.transform.position = Vector3.Lerp(startingPos, startingPos + slidingOffset, EaseInQuad(slideInTimer / slideInTimerMax));//TODO: EASING
+        if (slideInTimer > 0) slideInTimer -= Time.fixedDeltaTime;
+        else slideInTimer = 0;
+    }
+
+    ///PERSISTENCE ON SCREEN
+    private void HandlePersistence()
+    {
+        if (persistenceTimer > 0) persistenceTimer -= Time.fixedDeltaTime;
+        else this.gameObject.SetActive(false);
+    }
+
+
+
+
+
 
     //EVENT-HANDLING
     private void HandleMoossionCompletion(object sender, MoossionCompleteEventArgs e)
@@ -60,5 +93,9 @@ public class MoossionFeedItem : MonoBehaviour
         feedItemText.text = "Moossion #" + e.MoossionIndex + " complete!";
         this.gameObject.SetActive(true);
     }
+
+
+    //EASING
+    public static float EaseInQuad(float t) => t * t;
 
 }
