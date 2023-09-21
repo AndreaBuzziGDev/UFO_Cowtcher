90101894ab99791919378ce9d5010c2be58a9087
Andrea Buzzi <aby.development@outlook.it>
2023-09-21 19:18:47 +0200

    Implementing changes to make it work for all types of cows

--- Assets/Scripts/Movement Patterns/ScriptableObjects/MPAlertTowardsUFOSO.cs
+++ Assets/Scripts/Movement Patterns/ScriptableObjects/MPAlertTowardsUFOSO.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Alert Towards UFO", menuName = "MovementPattern/Alert/Towards UFO")]
 public class MPAlertTowardsUFOSO : MPAbstractAlertSO
 {
+    [SerializeField] public float timerMoving = 2f;
+    [SerializeField] public float timerStill = 1f;
+    [SerializeField] [Range(0, 1.0f)] public float randomizerSlider = 0.5f;
+
     public override AbstractMovementPattern GetMovPattern()
     {
         return new MPAlertTowardsUFO(this);
