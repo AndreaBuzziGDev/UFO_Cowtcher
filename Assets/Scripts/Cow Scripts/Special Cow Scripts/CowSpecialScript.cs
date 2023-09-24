8a2602579189eda611e3e8b619e410de7bf82d02
Andrea Buzzi <aby.development@outlook.it>
2023-09-24 20:58:45 +0200

    Implementing Cow Special Script and stuff

--- Assets/Scripts/Cow Scripts/Special Cow Scripts/CowSpecialScript.cs
+++ Assets/Scripts/Cow Scripts/Special Cow Scripts/CowSpecialScript.cs
@@ -2,28 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CowSpecialScript : MonoBehaviour
+public abstract class CowSpecialScript : MonoBehaviour
 {
     //DATA
     [SerializeField] private Cow myCow;
     [SerializeField] private CowMovement myMovement;
 
     [SerializeField] private float specialEffectActivationTimerMax = 10.0f;
+    private float specialEffectActivationTimer;
 
 
 
     //METHODS
     //...
 
-    // Start is called before the first frame update
-    void Start()
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        
+        if (myCow != null)
+        {
+            HandleSpecialEffectLogic();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    //FUNCTIONALITIES
+    private void HandleSpecialEffectLogic()
     {
-        
+        if (myCow.IsAlert)
+        {
+            if (specialEffectActivationTimer > 0)
+                specialEffectActivationTimer -= Time.fixedDeltaTime;
+            else
+            {
+                specialEffectActivationTimer = specialEffectActivationTimerMax;
+                HandleDedicatedBehaviour();
+            }
+        }
+        else if (myCow.IsCalm)
+        {
+            specialEffectActivationTimer = specialEffectActivationTimerMax;
+            //TODO: DISABLE SPECIAL SCRIPT
+
+        }
     }
+
+
+    //ABSTRACT METHODOLOGY
+    protected abstract void HandleDedicatedBehaviour();
+
 }
