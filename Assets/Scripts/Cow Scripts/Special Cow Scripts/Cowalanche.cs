ec4be84e16ffb1695c2126a4b26523841fbd4339
Andrea Buzzi <aby.development@outlook.it>
2023-09-25 02:48:27 +0200

    Implementing Cowalanche, WIP

--- Assets/Scripts/Cow Scripts/Special Cow Scripts/Cowalanche.cs
+++ Assets/Scripts/Cow Scripts/Special Cow Scripts/Cowalanche.cs
@@ -2,17 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cowalanche : MonoBehaviour
+public class Cowalanche : CowSpecialScript
 {
+    //DATA
+    [SerializeField] private float avalancheDuration = 5.0f;
+    [SerializeField] [Range(0, 100)] private float cowSpeedIncreasePercent = 50;
+    [SerializeField] private float specialEffectActivationTimerMax = 3.0f;//TODO: THIS IS THE DELAY, SHOULD ALSO USE A DIFFERENT ITEM TO IMPLEMENT THE SUBSEQUENT DELAYS?
+    private float specialEffectActivationTimer;
+
+    ///TECHNICAL DATA
+    private UFO playerUFO;
+    private PlayerController playerController;
+
+
+
+    //METHODS
     // Start is called before the first frame update
     void Start()
     {
-        
+        //REFERENCES
+        playerUFO = GameController.Instance.FindUFOAnywhere();
+        playerController = GameController.Instance.FindPlayerAnywhere();
+
+        //
+        specialEffectActivationTimer = specialEffectActivationTimerMax;
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    //ABSTRACT CONCRETIZATION
+
+    ///BEHAVIOUR
+    protected override void HandleDedicatedBehaviour()
     {
-        
+        //IF WITHIN ALERT RADIUS
+        if ((myCow.transform.position - playerUFO.GetPositionXZ()).magnitude < myCow.AlertRadius)
+        {
+            //HANDLE A TIMER
+            if (specialEffectActivationTimer > 0)
+                specialEffectActivationTimer -= Time.fixedDeltaTime;
+            else
+            {
+                specialEffectActivationTimer = specialEffectActivationTimerMax;
+
+                //SPECIAL EFFECT - APPLY STUN
+                ApplySpecialEffect(this.avalancheDuration);
+            }
+        }
     }
+
+
+    protected override void DisableDedicatedBehaviour()
+    {
+        //TODO: CHECK IF IT NEEDS IMPLEMENTATION
+
+    }
+
+    //FUNCTIONALITIES
+    public static void ApplySpecialEffect(float terrorDuration)
+    {
+        //TODO: IMPLEMENT
+
+        //APPLY TERROR TO PLAYER (FIRE EVENT?) - SHOULD IT BE PAIRED WITH A COROUTINE TO HANDLE RE-ITERATED TERROR?
+        //APPLY TERROR TO COWS (FIRE EVENT?) - SHOULD IT BE PAIRED WITH A COROUTINE TO HANDLE RE-ITERATED TERROR?
+
+        //playerController.ApplyStun(this.stunDuration);
+        UIController.Instance.IGPanel.DebuffPanel.fadeToTransparent = true;
+
+        //TODO: DO SOME VISUAL EFFECTS ON THE COW?
+
+    }
+
 }
