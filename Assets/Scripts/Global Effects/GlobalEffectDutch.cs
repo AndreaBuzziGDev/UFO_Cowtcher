31dcde1efdf5c8c244a31f9aabb829b6edaf856d
Andrea Buzzi <aby.development@outlook.it>
2023-09-25 21:23:01 +0200

    Implementing Curse

--- Assets/Scripts/Global Effects/GlobalEffectDutch.cs
+++ Assets/Scripts/Global Effects/GlobalEffectDutch.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class GlobalEffectDutch : MonoBehaviour
+public class GlobalEffectDutch : MonoSingleton<GlobalEffectDutch>
 {
     //DATA
+
+
+    List<Cow> disappearedCows = new();
 
 
     //METHODS
@@ -24,6 +28,38 @@ public class GlobalEffectDutch : MonoBehaviour
 
 
     //FUNCTIONALITIES
+    public void ApplyCurse(float curseDuration)
+    {
+        //MAKE ALL COWS THAT ARE NOT COWTCHMAN DISAPPEAR
 
+        //
+        List<Cow> allFoundCows = FindObjectsOfType<Cow>().ToList();
+
+        foreach(Cow fCow in allFoundCows)
+        {
+            if (fCow.gameObject.activeSelf && fCow.CowTemplate.UID != CowSO.UniqueID.L003_Flying_Cowtchman)
+            {
+                disappearedCows.Add(fCow);
+                fCow.gameObject.SetActive(false);
+            }
+        }
+
+        StartCoroutine(CurseRoutine(curseDuration));
+    }
+
+
+
+    //COROUTINES
+    private IEnumerator CurseRoutine(float curseDuration)
+    {
+        //WAIT FOR TIME
+        yield return new WaitForSeconds(curseDuration);
+
+
+        //RE-ENABLE FOG-DISABLED COWS
+        foreach(Cow dCow in disappearedCows)
+            dCow.gameObject.SetActive(true);
+
+    }
 
 }
