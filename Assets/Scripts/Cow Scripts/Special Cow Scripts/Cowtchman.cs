da97ca80ec0babf6899a24b8527f18cdb5048804
Andrea Buzzi <aby.development@outlook.it>
2023-09-25 02:31:14 +0200

    Implementing Cowtchman script

--- Assets/Scripts/Cow Scripts/Special Cow Scripts/Cowtchman.cs
+++ Assets/Scripts/Cow Scripts/Special Cow Scripts/Cowtchman.cs
@@ -2,17 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cowtchman : MonoBehaviour
+public class Cowtchman : CowSpecialScript
 {
+    //DATA
+    [SerializeField] private float curseDuration = 1.0f;
+    [SerializeField] private float specialEffectActivationTimerMax = 5.0f;
+    private float specialEffectActivationTimer;
+
+    ///TECHNICAL DATA
+    private UFO playerUFO;
+    private PlayerController playerController;
+
+
+
+    //METHODS
     // Start is called before the first frame update
     void Start()
     {
-        
+        //REFERENCES
+        playerUFO = GameController.Instance.FindUFOAnywhere();
+        playerController = GameController.Instance.FindPlayerAnywhere();
+
+        //
+        specialEffectActivationTimer = specialEffectActivationTimerMax;
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    //ABSTRACT CONCRETIZATION
+
+    ///BEHAVIOUR
+    protected override void HandleDedicatedBehaviour()
     {
-        
+        //IF WITHIN ALERT RADIUS
+        if ((myCow.transform.position - playerUFO.GetPositionXZ()).magnitude < myCow.AlertRadius)
+        {
+            //HANDLE A TIMER
+            if (specialEffectActivationTimer > 0)
+                specialEffectActivationTimer -= Time.fixedDeltaTime;
+            else
+            {
+                specialEffectActivationTimer = specialEffectActivationTimerMax;
+
+                //SPECIAL EFFECT - APPLY STUN
+                ApplySpecialEffect();
+            }
+        }
     }
+
+
+    protected override void DisableDedicatedBehaviour()
+    {
+        //TODO: CHECK IF IT NEEDS IMPLEMENTATION
+
+    }
+
+    //FUNCTIONALITIES
+    public void ApplySpecialEffect()
+    {
+        //APPLY CURSE TO PLAYER (FIRE EVENT INSTEAD?)
+
+        //USE COROUTINE TO HANDLE THE DISAPPEARANCE OF COWS?
+
+        //playerController.ApplyStun(this.stunDuration);
+        UIController.Instance.IGPanel.DebuffPanel.fadeToTransparent = true;
+
+        //TODO: DO SOME VISUAL EFFECTS ON THE COW?
+
+    }
+
+
+
+    //COROUTINE
+
+
 }
