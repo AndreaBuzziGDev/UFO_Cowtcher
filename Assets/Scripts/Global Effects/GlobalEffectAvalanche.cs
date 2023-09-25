eab2fa34e8bc047378e952c59261bc76656f1bf2
Andrea Buzzi <aby.development@outlook.it>
2023-09-25 21:50:25 +0200

    Handling Cowalanche movement speed bonus

--- Assets/Scripts/Global Effects/GlobalEffectAvalanche.cs
+++ Assets/Scripts/Global Effects/GlobalEffectAvalanche.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalEffectAvalanche : MonoBehaviour
+public class GlobalEffectAvalanche : MonoSingleton<GlobalEffectAvalanche>
 {
+    //DATA
+    private float avalancheSpeedMultiplier = 100.0f;
+    public float AvalancheSpeedMult { get { return avalancheSpeedMultiplier / 100.0f; } }
+    public bool IsAvalanche { get { return AvalancheSpeedMult > 1.0f; } }
+
+
+
+    //METHODS
+    //...
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,27 @@ public class GlobalEffectAvalanche : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
+    //FUNCTIONALITIES
+    public void ApplyAvalanche(float avalancheDuration, float speedBonusPercent)
+    {
+        avalancheSpeedMultiplier += speedBonusPercent;
+        StartCoroutine(AvalancheRoutine(avalancheDuration));
+    }
+
+
+
+    //COROUTINES
+    private IEnumerator AvalancheRoutine(float avalancheDuration)
+    {
+        //WAIT FOR TIME
+        yield return new WaitForSeconds(avalancheDuration);
+
+        //RE-SET SPEED
+        avalancheSpeedMultiplier = 100.0f;
+    }
+
 }
