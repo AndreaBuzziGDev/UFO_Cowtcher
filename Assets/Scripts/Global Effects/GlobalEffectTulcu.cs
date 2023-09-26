89047d135e299e57370f2d13ea7034acc0e4cc75
Andrea Buzzi <aby.development@outlook.it>
2023-09-26 16:18:21 +0200

    Global Effect Rain has been implemented

--- Assets/Scripts/Global Effects/GlobalEffectTulcu.cs
+++ Assets/Scripts/Global Effects/GlobalEffectTulcu.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalEffectTulcu : MonoBehaviour
+public class GlobalEffectTulcu : MonoSingleton<GlobalEffectTulcu>
 {
+    //DATA
+    [SerializeField] List<ParticleSystem> rainFalls;
+    [SerializeField] List<ParticleSystem> rainSplashes;
+
+
+    //METHODS
+    //...
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayRain();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+     
         
     }
+
+
+    //FUNCTIONALITIES
+    private void PlayRain()
+    {
+        foreach (ParticleSystem rf in rainFalls)
+            rf.Play();
+        foreach (ParticleSystem rs in rainSplashes)
+            rs.Play();
+    }
+
 }
