e3f4296bc3725881c0f23efeba9f657fc97d1d70
Andrea Buzzi <aby.development@outlook.it>
2023-09-27 12:09:36 +0200

    Implementing Easing in of main menu

--- Assets/Scripts/UI Scripts/Opening Menu/Opening Fading.cs
+++ Assets/Scripts/UI Scripts/Opening Menu/Opening Fading.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpeningFading : MonoBehaviour
 {
+    //DATA
+    [SerializeField] private Image myLogo;
+    private Color fullColor;
+    private Color transparentColor;
+
+    [SerializeField] private float fullLogoEasingTimeMax = 2.0f;
+
+    //METHODS
+    //...
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fullColor = myLogo.color;
+        transparentColor = new Color(fullColor.r, fullColor.g, fullColor.b, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        myLogo.color = Color.Lerp(transparentColor, fullColor, EaseInQuad(Time.time/fullLogoEasingTimeMax));
     }
+
+    //EASING
+    public static float EaseInQuad(float t) => t * t;
+
 }
